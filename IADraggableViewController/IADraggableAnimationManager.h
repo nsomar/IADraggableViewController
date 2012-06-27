3dd411e2f1cd76093bf484d4d3d2676e2efa547a
omar abdelhafith <ed.abdelhafith@gmail.com>
2012-06-27 23:27:31 +0300

    Fixing name

--- IADraggableViewController/IADraggableAnimationManager.h
+++ IADraggableViewController/IADraggableAnimationManager.h
@@ -2,7 +2,7 @@
 //  IADraggableAnimationManager.h
 //  AppleSlideView
 //
-//  Created by Omar Subhi on 6/23/12.
+//  Created by Omar Abdelhafith on 6/23/12.
 //  Copyright (c) 2012 Infusion Apps. All rights reserved.
 //
 
